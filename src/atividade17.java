321b158f08815a0ea8dfcd9a8e097c83bc300f86
jabes <138511008+jabesz@users.noreply.github.com>
2023-09-05 10:56:20 -0300

    Update atividade17.java

--- src/atividade17.java
+++ src/atividade17.java
@@ -3,12 +3,12 @@ import javax.swing.*;
 public class atividade17 {
     public static void main(String[] args) {
 
-        double raio = Double.parseDouble(JOptionPane.showInputDialog("| Informe o valor do raio do circulo |\n"));
-        double comprimento = 2 * Math.PI * raio;
+        int raio = Integer.parseInt(JOptionPane.showInputDialog("| Informe o valor do raio do circulo |\n"));
+        int comprimento = (int) 2 * Math.PI * raio;
 
-        double area = Math.PI * raio * raio;
+        int area = (int) Math.PI * raio * raio;
 
-        double volume = (4.0 / 3.0) * Math.PI * Math.pow(raio, 3);
+        int volume = (int) ((int) 4.0 / 3.0) * Math.PI * Math.pow(raio, 3);
 
         JOptionPane.showMessageDialog(null, "| O comprimento da esfera |\n" +comprimento);
         JOptionPane.showMessageDialog(null, "| A area da esfera é |\n" +area);
